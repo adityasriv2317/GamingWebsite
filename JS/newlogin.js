d1be060adbfa19d709a964756f8459d800631c21
adityasriv2317 <adityaxia9237@gmail.com>
2024-10-16 00:33:39 +0530

    rating button

--- JS/newlogin.js
+++ JS/newlogin.js
@@ -1,12 +1,14 @@
-let btna = document.getElementById('btn')
-let up = document.getElementById('upass').value
-let cp = document.getElementById('cpass').value
+document.addEventListener("DOMContentLoaded", function () {
+    let btna = document.getElementById('btn')
+    let up = document.getElementById('upass')
+    let cp = document.getElementById('cpass')
 
-btna.onclick = function () {
-    if (up == cp){
-        window.alert(1);
+    btna.onclick = function () {
+        if (up.value == cp.value){
+            // window.alert(1);
+        }
+        else{
+            window.alert("Passwords do not match");
+        }
     }
-    else{
-        window.alert(0);
-    }
-}
+})
---
+++ JS/star.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let ratings = document.querySelectorAll(".rating");
+
+    ratings.forEach(rate => {
+        let star = rate.querySelectorAll(".s");
+
+        star.forEach((st, vl) =>{
+            st.addEventListener("click", function() {
+                star.forEach(i => {
+                    i.querySelector(".full").style.display = "none";
+                });
+
+                for (let j = 0; j<= vl; j++){
+                    star[j].querySelector(".full").style.display = "block"
+                }
+            })
+        })
+    })
+})
