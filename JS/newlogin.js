62c1b2a582a2fdd52d16de972a0bd1c44a12864f
adityasriv2317 <adityaxia9237@gmail.com>
2024-10-16 02:03:43 +0530

    user page ui fix

--- JS/newlogin.js
+++ JS/newlogin.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     let btna = document.getElementById('btn')
+    let show_pass = document.getElementById('shpass')
+
     let up = document.getElementById('upass')
     let cp = document.getElementById('cpass')
 
@@ -9,6 +11,15 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         else{
             window.alert("Passwords do not match");
+        }
+    }
+
+    show_pass.onclick = function () {
+        if (up.type == 'text'){
+            up.type = 'password'
+        }
+        else{
+            up.type = 'text'
         }
     }
 })
