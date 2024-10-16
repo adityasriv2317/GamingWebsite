ec92d73b3f342d895cbbdb59d0ac4b8c9a6a634a
adityasriv2317 <adityaxia9237@gmail.com>
2024-10-17 01:05:36 +0530

    email check v3

--- JS/email.js
+++ JS/email.js
@@ -3,17 +3,29 @@ document.addEventListener('DOMContentLoaded', function(){
     let btn = document.querySelectorAll('.mcheck')
 
     let sym = true
+    let d = 0
 
     btn.forEach((b, i) =>{
         b.onclick = function(){
             sym = false
+            d =0
             let m = em[i]
 
             for(let j=1; j<m.value.length - 2; j++){
                 let ch = m.value.charCodeAt(j)
 
                 if(ch == 64){
-                    sym = true
+                    if(!sym && d <= i){
+                        d = i
+                        sym = true
+                    }
+                    else if(sym){
+                        sym = false
+                        break
+                    }
+                    else{
+                        break
+                    }
                 }
             }
 
