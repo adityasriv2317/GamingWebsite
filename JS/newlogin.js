a7a3186ccd50a319787f5d12ffd3f01be4cdc56a
adityasriv2317 <adityaxia9237@gmail.com>
2024-10-16 12:22:44 +0530

    login structure fix

--- JS/newlogin.js
+++ JS/newlogin.js
@@ -1,25 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let btna = document.getElementById('btn')
-    let show_pass = document.getElementById('shpass')
-
+    let chk = document.getElementById('btn')
     let up = document.getElementById('upass')
     let cp = document.getElementById('cpass')
 
-    btna.onclick = function () {
-        if (up.value == cp.value){
-            // window.alert(1);
+    chk.onclick = function () {
+        if (up.value != cp.value){
+            window.alert("Passwords do not match");
         }
         else{
-            window.alert("Passwords do not match");
+            console.log('same pass')
         }
     }
 
-    show_pass.onclick = function () {
-        if (up.type == 'text'){
-            up.type = 'password'
-        }
-        else{
-            up.type = 'text'
-        }
-    }
 })
